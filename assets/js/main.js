eeb578769213122723a9aea8004228f0ae8d488c
Marcus Vinícius Lara <marcus.lara@hotmail.com>
2023-04-18 17:58:51 -0300

    Integrando a API

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,12 @@
-console.log("Sucesso");
+const offSet = 0;
+const limit = 20;
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offSet}&limit=${limit}`
+
+fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => console.log(jsonBody))
+    .catch((error) => console.log(error))
+    
+
+
+    
